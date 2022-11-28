82b2b0b527a9192ac65dab56191eb7088f623391
Siyuan Liu <sssiu@ucdavis.edu>
2022-11-27 22:13:59 -0800

    Code Display

--- webapp/ClientApp/src/components/OperationModal.js
+++ webapp/ClientApp/src/components/OperationModal.js
@@ -1,6 +1,7 @@
 
 import React from 'react';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import { CopyBlock } from "react-code-blocks"
 
 class OperationModal extends React.Component {
   constructor(props) {
@@ -23,12 +24,13 @@ class OperationModal extends React.Component {
         <Modal isOpen={this.isOpen()} toggle={this.toggle} className={this.props.className}>
           <ModalHeader toggle={this.toggle}>{this.props.kvStatus.name}</ModalHeader>
           <ModalBody>
-            {this.props.kvStatus.content}
+            <CopyBlock 
+              text={this.props.kvStatus.content ? this.props.kvStatus.content : ""}
+              language="javascript"
+              showLineNumbers={false}
+              theme="dracula"
+            />
           </ModalBody>
-          <ModalFooter>
-            <Button color="primary" onClick={this.toggle}>Do Something</Button>{' '}
-            <Button color="secondary" onClick={this.toggle}>Cancel</Button>
-          </ModalFooter>
         </Modal>
       </div>
     );
