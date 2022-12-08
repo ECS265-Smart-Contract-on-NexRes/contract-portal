6b84ec7ad1c3e1246b0a84ef31bd21e092bda4b2
uilnauyis_DP <sssiu@ucdavis.edu>
2022-12-08 13:32:30 -0800

    login and display contract

--- webapp/ClientApp/src/_actions/user.actions.js
+++ webapp/ClientApp/src/_actions/user.actions.js
@@ -6,7 +6,7 @@ import { authAtom, usersAtom } from '../_state';
 export { useUserActions };
 
 function useUserActions () {
-    const baseUrl = `${process.env.REACT_APP_API_URL}/users`;
+    const baseUrl = `api/users`;
     const fetchWrapper = useFetchWrapper();
     const setAuth = useSetRecoilState(authAtom);
     const setUsers = useSetRecoilState(usersAtom);
--- webapp/ClientApp/src/components/Home.js
+++ webapp/ClientApp/src/components/Home.js
@@ -1,27 +1,35 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect, useState } from 'react';
 import { Button } from 'reactstrap';
 import OperationModal from './OperationModal';
-export class Home extends Component {
-  static displayName = Home.name;
+import { useFetchWrapper } from '../_helpers/fetchWrapper';
 
-  constructor(props) {
-    super(props);
-    this.state = { kvStatus: [], loading: true, selectedKey: null };
+export function Home() {
+  const displayName = Home.name;
+  const fetchWrapper = useFetchWrapper();
+  const [kvStatus, setKvStatus] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [selectedKey, setSelectedKey] = useState(null);
+
+  useEffect(() => {
+    populateContractData();
+  }, [])
+
+  const populateContractData = async function () {
+    const response = await fetchWrapper.get('api/binary/list');
+    const data = response;
+    setKvStatus(data);
+    setLoading(false);
   }
 
-  componentDidMount() {
-    this.populateWeatherData();
+  const undoSelect = function () {
+    setSelectedKey(null);
   }
 
-  undoSelect() {
-    this.setState({...this.state, selectedKey: null});
-  }
-
-  renderBinariesTable(kvStatus) {
+  const renderBinariesTable = function (kvStatus) {
     return (
       <div>
         {kvStatus.map((item) =>
-          <OperationModal key={item.key} selectedKey={this.state.selectedKey} undo={this.undoSelect.bind(this)} modal={false} kvStatus={item}></OperationModal>
+          <OperationModal key={item.key} selectedKey={selectedKey} undo={undoSelect.bind(this)} modal={false} kvStatus={item}></OperationModal>
         )}
         <table className='table table-striped' aria-labelledby="tabelLabel">
           <thead>
@@ -34,7 +42,7 @@ export class Home extends Component {
           <tbody>
             {kvStatus.map((item) =>
               <tr
-                onClick={() => {this.setState({...this.state, selectedKey: item.key})}}
+                onClick={() => { setSelectedKey(item.key) }}
                 key={item.key}>
                 <td>{item.name}</td>
                 <td>{item.key}</td>
@@ -47,22 +55,13 @@ export class Home extends Component {
     );
   }
 
-  render() {
-    let contents = this.state.loading
-      ? <p><em>Loading...</em></p>
-      : this.renderBinariesTable(this.state.kvStatus);
 
-    return (
-      <div>
-        <h1 id="tabelLabel" >Smart Contracts</h1>
-        {contents}
-      </div>
-    );
-  }
-
-  async populateWeatherData() {
-    const response = await fetch('api/binary/list');
-    const data = await response.json();
-    this.setState({ kvStatus: data, loading: false });
-  }
+  return (
+    <div>
+      <h1 id="tabelLabel" >Smart Contracts</h1>
+      {loading
+        ? <p><em>Loading...</em></p>
+        : renderBinariesTable(kvStatus)}
+    </div>
+  );
 }
