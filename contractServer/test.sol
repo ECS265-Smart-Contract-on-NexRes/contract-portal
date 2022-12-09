57452a7b1281372de9252ecd75c922ecf4838177
uilnauyis_DP <sssiu@ucdavis.edu>
2022-12-08 22:37:58 -0800

    Add smart contract test.sol

---
+++ contractServer/test.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.6.6;
+contract Vault {
+    uint vaultData;
+    uint money;
+    function set(uint data) public{
+        vaultData = data;
+    }
+    
+    function add(uint data) public{
+        vaultData = vaultData + data;
+    }
+    
+    function minus(uint data) public{
+        vaultData = vaultData - data;
+    }
+
+    function get() public view returns (uint) {
+        return vaultData;
+    }
+}
